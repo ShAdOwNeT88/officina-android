8bf378cbae2ba1c4d3b0f8dd364955e6ce08a52e
Iovine Antonio Francesco <iovine.antonio.1988@gmail.com>
2023-02-18 14:21:25 +0100

    added location in json and MonumentConfig domain model

--- app/src/main/java/com/officinasocialeproarpaia/officina_android/features/main/domain/MonumentConfig.kt
+++ app/src/main/java/com/officinasocialeproarpaia/officina_android/features/main/domain/MonumentConfig.kt
@@ -11,10 +11,16 @@ data class MonumentConfig(
         @SerializedName("trackStartRange") val trackStartRange: Double,
         @SerializedName("trackStopRange") val trackStopRange: Double,
         @SerializedName("monumentName") val monumentName: String,
+        @SerializedName("location") val location: Location?,
         @SerializedName("monumentSubtitles") val monumentSubtitles: List<MonumentSubtitle>,
         @SerializedName("monumentAudioUrls") val monumentAudioUrls: List<MonumentAudioUrl>,
         @SerializedName("monumentDescriptions") val monumentDescriptions: List<MonumentDescription>,
     ) {
+        data class Location(
+            @SerializedName("latitude") val latitude: Double,
+            @SerializedName("longitude") val longitude: Double,
+        )
+
         data class MonumentSubtitle(
             @SerializedName("language") val language: String,
             @SerializedName("subtitle") val subtitle: String,
